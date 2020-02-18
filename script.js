1eb90b793552daff9ac629842c1aaaf29551e40c
Vinicius Leal <vinicius.leal1999@outlook.com>
2020-02-18 17:55:05 -0300

    master

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ function verificar() {
     var data = new Date()
     ano = data.getFullYear()
     var nasc = document.getElementById('idcampoidade').value;
-    idade = ano-nasc;
+    idade = ano - nasc;
 
+    window.alert(nasc)
+    window.alert(`Você tem ${idade}` + ' anos')
+    if (nasc > ano || nasc.length() == null) {
+        window.alert('[ERRO] - Verifique os dados e tente novamente')
+    }
+
+    
     var feminino = document.getElementById('masc')
     var masculino = document.getElementById('fem')
-    
 
+    window.alert(feminino)
+    window.alert(masculino)
     window.alert(idade + ' anos');
-    if(idade && sexo){}
+
     //else if() {}
 }   
