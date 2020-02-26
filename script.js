c33837e46c574a08142e294bdb13e554036613a6
Vinicius Leal <vinicius.leal1999@outlook.com>
2020-02-26 00:17:50 -0300

    VerificadorDeIdade Finished. v1

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 function verificar() {
-
     var data = new Date()
     ano = data.getFullYear()
     var nasc = document.getElementById('idcampoidade').value;
     idade = ano - nasc;
 
-    window.alert(nasc)
-    window.alert(`Você tem ${idade}` + ' anos')
-    if (nasc > ano || nasc.length() == null) {
-        window.alert('[ERRO] - Verifique os dados e tente novamente')
+    if (nasc > ano || nasc.length == null || nasc < 1900) {
+        window.alert('[ERRO] - Verifique a data informada e tente novamente')
+    } else {
+        var sexo = document.getElementsByName('sexo')
+        var genero = ''
+        var img = document.createElement('img')
+        if (sexo[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 2) {
+                img.setAttribute('src', 'bebe.png')
+            } else if (idade < 12) {
+                img.setAttribute('src', 'images/crianca.png')
+            } else if (idade < 20) {
+                img.setAttribute('src', 'images/menino.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'images/homem.png')
+            } else {
+                img.setAttribute('src', 'images/velho.png')
+            }
+        }
+        else if (sexo[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 2) {
+                img.setAttribute('src', 'images/bebemenina.png')
+            } else if (idade <15) {
+                img.setAttribute('src', 'images/menina.png')
+            } else if (idade <22) {
+                img.setAttribute('src', 'images/moca.png')
+            } else if (idade < 48) {
+                img.setAttribute('src', 'images/mulher.png')
+            } else {
+                img.setAttribute('src', 'images/velha.png')
+            }
+        }
+        if(idade == 1){
+            res.innerHTML = `Detectamos um ${genero} com ${idade} ano`
+        } else {
+            res.innerHTML = `Detectamos um ${genero} com ${idade} anos`
+        }
+       
+        res.appendChild(img)
     }
 
-    
-    var feminino = document.getElementById('masc')
-    var masculino = document.getElementById('fem')
 
-    window.alert(feminino)
-    window.alert(masculino)
-    window.alert(idade + ' anos');
-
-    //else if() {}
-}   
+}
